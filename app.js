dc2891fd4967d60e4ae625ad7292d28e2dd6a535
Levin Batallones <levinbatallones@Batallones.local>
2020-07-17 14:09:31 -0700

    Add 5 different data types

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log("hello")
+//5 variables of different data types
+//string
+let name = "Levin";
+console.log(name)
+//Number
+let myNumber = 27;
+console.log(myNumber)
+//Array 
+const myArray = [1, 2, 3, 4];
+console.log(myArray)
+//boolean 
+let myBool = true;
+let myBoolTwo = false;
+console.log(myBool)
+console.log(myBoolTwo)
+//object with 4 different key and pairs [string], [number], [array], [boolean]
+
+const myObj = {
+    Name: 'Levin',
+    Male: true,
+    favoriteSports:['Basketball', 'Football'],
+    age: 24
+}
+console.log(myObj)
